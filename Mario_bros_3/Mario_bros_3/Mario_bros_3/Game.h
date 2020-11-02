117bf9749e28f7668272e411aa07d2da5418a349
Dokkaebi00 <quang009182@gmail.com>
2020-11-02 19:14:15 +0700

    add WinUser.h, Windows.h

--- Mario_bros_3/Mario_bros_3/Mario_bros_3/Game.h
+++ Mario_bros_3/Mario_bros_3/Mario_bros_3/Game.h
@@ -1,7 +1,9 @@
-#include <dinput.h> 
-#include <d3d9.h>
+#include<Windows.h>
+#include<dinput.h> 
+#include<d3d9.h>
 #include<d3dx9.h>
-#include <InitGuid.h>
+#include<InitGuid.h>
+#include<WinUser.h>
 
 //#include "define.h"
 //#include "SceneManager.h"
